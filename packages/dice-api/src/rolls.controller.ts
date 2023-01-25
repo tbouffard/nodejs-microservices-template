0f8f98fe2dae7243bad12e9a62baa2d285d50e43
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-01-25 13:56:43 +0000

    dice: POST validate parameter

--- packages/dice-api/src/rolls.controller.ts
+++ packages/dice-api/src/rolls.controller.ts
@@ -17,7 +17,7 @@ export class RollsController {
   constructor(private readonly db: DbService) {}
 
   @Post()
-  async rollDice(@Body('sides') sides: number) {
+  async rollDice(@Body('sides', ParseIntPipe) sides: number) {
     this.logger.log(`Rolling dice [sides: ${sides}]`);
     const result = Math.ceil(Math.random() * sides);
     await this.db.addRoll({
