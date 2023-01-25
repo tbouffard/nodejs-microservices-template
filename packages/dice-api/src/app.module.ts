9d2648fc682b32a18da4c95bf9c466b72c506ec0
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-01-25 13:41:35 +0000

    dice: init project

--- packages/dice-api/src/app.module.ts
+++ packages/dice-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbService } from './db.service';
 
 @Module({
   imports: [LoggerModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DbService],
 })
 export class AppModule {}
 
