11a5488251de0c1c3b8744c32c504296f88b3d78
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-01-25 14:21:56 +0000

    gateway: use auth

--- packages/gateway-api/app.js
+++ packages/gateway-api/app.js
@@ -4,12 +4,15 @@ const pino = require('pino-http')();
 
 const router = require('./routes/index');
 
+const auth = require('./middlewares/auth');
+
 const app = express();
 
 app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 
 app.use('/api', router);
 
